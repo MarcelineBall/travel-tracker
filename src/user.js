b0c0b08b3a63edc1ca145907b5c48642856f3b19
Marceline Ball <marchettiball@gmail.com>
2021-04-22 11:13:24 -0600

    Write and pass all tests for user class

--- src/user.js
+++ src/user.js
@@ -1,6 +1,9 @@
 class User {
-  constructor () {
-
+  constructor (info) {
+    this.id = info.id;
+    this.name = info.name;
+    this.travelerType = info.travelerType;
+    this.trips = []
   }
 }
 
