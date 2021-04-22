d337e0bc4f080caad473c3f73e49cbe795d2840e
Marceline Ball <marchettiball@gmail.com>
2021-04-22 14:00:22 -0600

    Add tripRepo class and test

---
+++ src/tripRepo.js
@@ -0,0 +1,7 @@
+class TripRepo {
+  constructor(info) {
+    this.tripData = info
+  }
+}
+
+export default TripRepo
---
+++ test/tripRepo-test.js
@@ -0,0 +1,39 @@
+import TripRepo from '../src/tripRepo.js'
+import { expect } from 'chai'
+
+describe('trip class', function() {
+  let trip
+  let tripData = [
+    {
+      id: 134,
+      userID: 1,
+      destinationID: 50,
+      travelers: 6,
+      date: "2020/02/13",
+      duration: 6,
+      status:"approved",
+      suggestedActivities:[]
+    },
+    {
+      id: 4,
+      userID: 6,
+      destinationID: 12,
+      travelers: 3,
+      date: "2020/05/22",
+      duration: 6,
+      status:"pending",
+      suggestedActivities:[]
+    }
+  ]
+
+  beforeEach(function() {
+  tripRepo = new TripRepo(tripData)
+  })
+
+  it('should be a function', function() {
+    expect(TripRepo).to.be.a('function')
+  })
+
+  it('should be an instance of Trip', function() {
+    expect(tripRepo).to.be.an.instanceof(TripRepo)
+  })
