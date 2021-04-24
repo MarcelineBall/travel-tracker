772755781dfa7ed60e7e7f718e3c2565396c2479
Marceline Ball <marchettiball@gmail.com>
2021-04-24 10:08:34 -0600

    Move code from scripts file to index.js

--- src/index.js
+++ src/index.js
@@ -7,4 +7,36 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import TripRepo from "./tripRepo.js";
+import Trip from "./trip.js";
+import User from "./user.js";
+
+const tripPlannerSection = document.querySelector("#tripPlanner")
+let tripRepo
+let users
+let destinations
+
+window.addEventListener("click", getUsers);
+
+function getUsers() {
+  fetch('http://localhost:3001/api/v1/travelers')
+    .then(response => errorCheck(response))
+    .then(data => users = data)
+    .then(data => console.log(users))
+    .catch(error => errorCheck(error))
+}
+
+function loadDataFromAPI() {
+
+}
+
+function errorCheck(response) {
+  if (!response.ok) {
+    console.log('error check error')
+    tripPlannerSection.innerHTML(
+      `<h1>We're sorry, there appears to be an error. Please Try again later!</h1>`)
+  } else {
+    console.log('error check no error')
+    return response.json()
+  }
+}
--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,42 @@
-import "/.index.js";
-import TripRepo from "./tripRepo.js";
-import Trip from "./trip.js";
-import User from "./user.js";
-
-let tripRepo
-let user
+// import "/.index.js";
+// import TripRepo from "./tripRepo.js";
+// import Trip from "./trip.js";
+// import User from "./user.js";
+//
+// const tripPlannerSection = document.querySelector("#tripPlanner")
+// let tripRepo
+// let users
+// let destinations
+//
+// window.addEventListener("click", anything);
+//
+// function getUsers() {
+//   console.log('function is going')
+//   fetch('http://localhost:3001/api/v1/travelers')
+//     .then(response => errorCheck(response))
+//     .then(data => users = data)
+//     .then(data => console.log(users))
+//     .catch(error => errorCheck(error))
+// }
+//
+//
+// function anything() {
+//   console.log('anything function')
+// }
+// anything()
+// getUsers()
+// 
+// function loadDataFromAPI() {
+//
+// }
+//
+// function errorCheck(response) {
+//   if (!response.ok) {
+//     console.log('error check error')
+//     tripPlannerSection.innerHTML(
+//       `<h1>We're sorry, there appears to be an error. Please Try again later!</h1>`)
+//   } else {
+//     console.log('error check no error')
+//     return response.json()
+//   }
+// }
