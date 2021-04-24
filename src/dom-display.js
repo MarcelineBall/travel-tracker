02a2079bb0150216c7c9cb6a331d228e1ecf3231
Marceline Ball <marchettiball@gmail.com>
2021-04-24 11:34:33 -0600

    Build html card for dom display

--- src/dom-display.js
+++ src/dom-display.js
@@ -1,4 +1,17 @@
 let domDisplay = {
 
+  displayTripCard(trip, element) {
+    let cardHTML =
+    `
+      <article>
+      <p>Trip number: ${trip.id}</p></br>
+      <p>Destination: ${}</p></br>
+      <p>Departure: ${trip.date}</p></br>
+      <p>Travelers: ${trip.travelers}</p></br>
+      <p>Status: ${trip.status}</p></br>
+      </article>
+    `
+    element.insertAdjacentHTML('beforeend', cardHTML)
+  }
 }
 export default domDisplay
